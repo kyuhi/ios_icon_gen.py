b97a98e0a6b738cb21a1f3f6f393837e13534a86
kyuhi <kyuhi74apid@gmail.com>
2014-12-03 16:42:00 +0900

    add information

--- ios_icon_gen.py
+++ ios_icon_gen.py
@@ -3,21 +3,24 @@
 import os
 import sys
 
+# See https://developer.apple.com/library/ios/documentation/userexperience/conceptual/mobilehig/IconMatrix.html
 iphone_only = {
-  'iTunesArtwork' : (512, 512),
-  'iTunesArtwork@2x' : (1024, 1024),
+  'iTunesArtwork' : (512, 512), # 
+  'iTunesArtwork@2x' : (1024, 1024), # app icon for the app store
   'Icon-60@2x.png' : (120, 120),
+  'Icon-60@3x.png' : (180, 180),
   'Icon-76.png ' : (76, 76),
   'Icon-76@2x.png ' : (152, 152),
   'Icon-Small-40.png' : (40, 40),
   'Icon-Small-40@2x.png ' : (80, 80),
   'Icon-Small.png' : (29, 29),
   'Icon-Small@2x.png' : (58, 58),
+  'Icon-Small@3x.png' : (87, 87),
 }
 if len(sys.argv) > 1:
   orignal = sys.argv[1]
 else:
-  print( 'ios_icon_gen.py <filename>' )
+  print( 'ios_icon_gen.py <image to convert>' )
   quit(1)
 
 
